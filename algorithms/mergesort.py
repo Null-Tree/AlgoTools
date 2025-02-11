1cd05769e230870356f25f5076803daa77a2d45e
incelerity <stillherebutgone@gmail.com>
2025-02-11 19:28:55 +1100

    a

--- algorithms/mergesort.py
+++ algorithms/mergesort.py
@@ -5,20 +5,53 @@ input = [1,5,2,3,5,3,67,1]
 
 
 
-
-
-
-
-
-
-
-print()
-
 def sort(input:list) -> list:
+    print(1)
+    lengh = len(input)
+    if lengh <= 1:
+        return list
     
+    middle_index = lengh//2
+
+    leftList=input[:middle_index]
+    rightList=input[middle_index:]
+
+
+    sortedLeftList = sort(leftList)
+    sortedRightList = sort(rightList)
     
+    lLengh = len(sortedLeftList)
+    rLengh=len(sortedRightList)
+    result=[]
+    li,ri=0
+
+    for i in lengh:
+        
+        if li >= lLengh:
+            result.append(sortedRightList[i])
+            ri+=1
+        if ri >= rLengh:
+            result.append(sortedLeftList[i])
+            li+=1
+
+        # sort asending
+        if sortedLeftList[i] > sortedRightList[i]:
+            result.append(sortedRightList[i])
+            ri+=1
+        else:
+            result.append(sortedLeftList[i])
+            li+=1
+
+    return result
+
+
 
 
 
 def main():
-    sort(input)
+    lst=sort(input)
+    print(lst)
+
+
+
+main()
