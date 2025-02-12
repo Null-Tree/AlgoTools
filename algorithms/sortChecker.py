feb3339313d607572a30a7d57efafcdbbdf0a8d1
incelerity <stillherebutgone@gmail.com>
2025-02-12 18:47:14 +1100

    a

--- algorithms/sortChecker.py
+++ algorithms/sortChecker.py
@@ -6,16 +6,16 @@ import random
 
 def mainsort(input:list):
 ###############################################
-#please sort the input list and return the sorted list
+
+#     please sort the input list and return the sorted list
         
-    # quicksort.quickSort(unsortedList)
-    # sortedList=unsortedList
+    quicksort.quickSort(unsortedList)
+    sortedList=unsortedList
 
 
-    sortedList = alexquicksort.quicksort(input)
+    # sortedList = alexquicksort.quicksort(input)
     
     return sortedList
-
 ###############################################
 
 
@@ -26,10 +26,15 @@ def mainsort(input:list):
 # max lengh of list you wanna test
 maxLengh=100
 
+# number of random lists to test per list lengh
+trialsPerLengh = 3
+
 # what order to test for
 # options: "asc" "desc" "either"
 mode="either"
 
+# what time to test for, "real" or "process"
+timerMode = "real"
 
 
 
@@ -47,6 +52,13 @@ mode="either"
 
 
 
+################################################################################################################################################################################################
+
+#   #
+ # #
+  #
+ # #
+#   #
 
 ################################################################################################################################################################################################
 
@@ -117,48 +129,55 @@ if __name__=="__main__":
         raise Exception("please have maxLengh of 1 or heigher")
 
     for i in range(1,maxLengh+1):
+        for trial in range(trialsPerLengh):
 
-        unsortedList=[]
+            unsortedList=[]
 
-        for j in range(i):
+            for j in range(i):
 
-            unsortedList.append(random.randint(0,100))
+                unsortedList.append(random.randint(0,100))
 
-        
-        Rstarttime=time.perf_counter()
+            if timerMode== "real":
+                Rstarttime=time.perf_counter()
+                sortedList=mainsort(unsortedList.copy())
+                Rendtime = time.perf_counter()
+            elif timerMode == "process":
+                Rstarttime=time.process_time()
+                sortedList=mainsort(unsortedList.copy())
+                Rendtime = time.process_time()
+            else:
+                raise Exception("invalid timer mode :(")
+            
+            RtimeTaken= timedelta(seconds=Rendtime - Rstarttime)
 
-        sortedList=mainsort(unsortedList.copy())
+            print(f"Testing random list of lengh: {i}")
+            print(f"    trial:{trial+1}")
+            passed = True
 
-        Rendtime = time.perf_counter()
-        RtimeTaken= timedelta(seconds=Rendtime - Rstarttime)
+            # check order
+            if mode == "either":
+                if (not checkorder(sortedList,"asc")) and (not checkorder(sortedList,"desc")):
+                    print("incorrect order, accepting either order")
+                    printDebug(unsortedList,sortedList)
+                    passed = False
+            else:
+                if (not checkorder(sortedList,mode)):
+                    print(f"incorrect order, testing for {mode} order")
+                    printDebug(unsortedList,sortedList)
+                    passed = False
+            
 
-        print(f"Testing random list of lengh: {i}")
-        passed = True
-
-        # check order
-        if mode == "either":
-            if (not checkorder(sortedList,"asc")) and (not checkorder(sortedList,"desc")):
-                print("incorrect order, accepting either order")
+            if not checkListContent(sortedList,unsortedList):
+                print(f"Contents of received list did not match original list")
                 printDebug(unsortedList,sortedList)
                 passed = False
-        else:
-            if (not checkorder(sortedList,mode)):
-                print(f"incorrect order, testing for {mode} order")
-                printDebug(unsortedList,sortedList)
-                passed = False
+
+            print(f"    real time elapsed: {RtimeTaken}")
+            if passed:
+                print("    Pass")
+            else:
+                print("    No pass")
+                quit()
         
-
-        if not checkListContent(sortedList,unsortedList):
-            print(f"Contents of received list did not match original list")
-            printDebug(unsortedList,sortedList)
-            passed = False
-
-        print(f"    real time elapsed: {RtimeTaken}")
-        if passed:
-            print("    Pass")
-        else:
-            print("    No pass")
-            quit()
-    
 
     
